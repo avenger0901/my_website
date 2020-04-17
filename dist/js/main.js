a68cce73b94b87cc4678ab8880c5ab7548f20830
zhao ziyi <zhaoziy@oregonstate.edu>
2020-04-16 22:23:15 -0700

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,8 +2,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItem = document.querySelectorAll('.nav-item');
+const navItems = document.querySelectorAll('.nav-item');
 
 //Set Initial State Of Menu
 
@@ -16,16 +15,17 @@ function toggleMenu(){
     menuBtn.classList.add('close');
     menu.classList.add('show');
     menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItem.forEach(item => item.classList.add('show'));
+    navItems.forEach(item => item.classList.add('show'));
 
     //Set Menu State
     showMenu = true;
-  }else{
+  }else {
     menuBtn.classList.remove('close');
     menu.classList.remove('show');
     menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItem.forEach(item => item.classList.remove('show'));
+    navItems.forEach(item => item.classList.remove('show'));
+
+    //Set menu state
+    showMenu = false;
   }
 }
