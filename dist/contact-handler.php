f829ac31ddc6c6784f51548f0aeb9799feec4a03
zhao ziyi <zhaoziy@oregonstate.edu>
2020-04-28 21:49:33 -0700

    changed new form

--- dist/contact-handler.php
+++ dist/contact-handler.php
@@ -9,8 +9,8 @@
   $email_subject = "New Form Submission";
 
   $email_body = "User Name: $name.\n".
-                  "User Email: $visitor_email.\n".
-                  "User Message: $message.\n";
+                "User Email: $visitor_email.\n".
+                "User Message: $message.\n";
 
 
 
